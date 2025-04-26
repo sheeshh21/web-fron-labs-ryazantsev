3ec9dda9df2d9325bd9ee07bbea2065e96893490
Рязанцев Александр <alex.ryazantsev.80@mail.ru>
2025-04-26 19:07:34 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Харпер Ли',
+        name: 'Убить пересмешника',
+        year: 1960,
+        price: '(350₽)',
+        bestseller: false,
+        dostup: true,
+    },
+    {
+        author: 'Джейн Остен',
+        name: 'Гордость и предубеждение',
+        year: 1813,
+        price: '(400₽)',
+        bestseller: true,
+        dostup: false,
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        year: 1967,
+        price: '(550₽)',
+        bestseller: false,
+        dostup: false,
+    },
+    {  
+        author: "Джордж Оруэлл", 
+        name: "1984",
+        year: 1949, 
+        price: "(350₽)", 
+        bestseller: true, 
+        dostup: false 
+    },
+    { 
+        author: "Дж.К. Роулинг", 
+        name: "Гарри Поттер и философский камень", 
+        year: 1997, 
+        price: "(550₽)", 
+        bestseller: true, 
+        dostup: false 
+    },
+    { 
+        author: "Толстой", 
+        name: "Война и мир", 
+        year: 1869, 
+        price: "(590₽)", 
+        bestseller: false, 
+        dostup: false 
+    },
+    { 
+        author: "Брэдбери", 
+        name: "451 градус по Фаренгейту", 
+        year: 1953, 
+        price: "(470₽)", 
+        bestseller: false,
+        dostup: true 
+    },
+    { 
+        author: "Хемингуэй", 
+        name: "Старик и море", 
+        year: 1952, 
+        price: "(340₽)", 
+        bestseller: false, 
+        dostup: false 
+    },
+    { 
+        author: "Хоссейни", 
+        name: "Тысяча сияющих солнц", 
+        year: 2007, 
+        price: "(650₽)", 
+        bestseller: true, 
+        dostup: false 
+    },
+    { 
+        author: "Достоевский", 
+        name: "Преступление и наказание", 
+        year: 1866, 
+        price: "(600₽)", 
+        bestseller: false, 
+        dostup: true 
+    }
+]
